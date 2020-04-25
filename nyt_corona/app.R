262ea0efa25ff2c63cedd4060c839067a7b5756e
Jerrica Li <jerrica_li@college.harvard.edu>
2020-04-25 15:02:28 -0400

    starting plot for shiny app

--- nyt_corona/app.R
+++ nyt_corona/app.R
@@ -7,15 +7,68 @@ library(dplyr)
 library(ggthemes)
 library(plotly)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-    mainPanel(
-        imageOutput("preImage")
-    )
-)
+county_data_shiny <- readRDS("county_data")
+state_data_shiny <- readRDS("state_data")
+
+
+ui <- navbarPage(
+    "Coronavirus Cases in the US",
+    tabPanel("Model",
+             fluidPage(
+                 titlePanel("Modeling"),
+                 sidebarLayout(
+                     sidebarPanel(
+                         selectInput(inputId = 'state', label = 'Choose a state:', choice = " "
+                         )),
+                     mainPanel(plotOutput("stateplot")))
+             )),
+    tabPanel("Discussion",
+             titlePanel("Discussion Title"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Hello, this is where I talk about my project."),
+             h3("About Me"),
+             p("My name is ______ and I study ______. 
+             You can reach me at ______@college.harvard.edu.")))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    output$stateplot <- renderPlot({
+    
+    title <- "Confirmed coronavirus cases"
+    
+    # Return blank if no choice selected yet
+    
+    if (input$state == " ") {
+        return()
+    }
+    
+    # Filter the dataset based on what state was selected
+    
+    choice <- state_data_shiny %>%
+        filter(state == input$state) 
+    
+    #Create plot of corona cases over time
+    
+    ggplot(choice, aes(x = new_date, y = cases)) +
+        geom_line() +
+        geom_point() + 
+        theme_minimal() +
+
+        theme(axis.text.x = element_text(angle = 70, hjust = 1), 
+              panel.grid.major = element_blank()) + 
+        labs(title =paste("Confirmed Corona Cases for", input$state, sep = ""),  
+             x = "Date", 
+             y = "Confirmed Cases") +
+        scale_x_date(date_breaks = "2 days", date_labels = "%b %d") +
+        geom_area(mapping=aes(x=new_date), fill="#9898fb", alpha=.5) 
+    })
+    
+    #remember input$state
     
     
     output$preImage <- renderImage({
@@ -40,4 +93,6 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
 
