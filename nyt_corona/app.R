e70ed6b2dd79205ce01bd949be59009d219c1f3c
Jerrica Li <jerrica_li@college.harvard.edu>
2020-04-26 18:23:27 -0400

    add hover function

--- nyt_corona/app.R
+++ nyt_corona/app.R
@@ -25,14 +25,14 @@ ui <- navbarPage(
                          
                          selectInput(inputId = 'state', 
                                      label = 'Choose a state:', 
-                                     choice = levels(state_data_shiny$state))),
+                                     choice = levels(state_data_shiny$state)
+                                     )
+                         ),
                      
                      #output state plot and death plot
-                     mainPanel(plotOutput("stateplot", 
-                                          hover = "stateplot_hover"), 
-                               plotOutput("deathplot", 
-                                          hover = "deathplot_hover")),  
-                     verbatimTextOutput("info")
+                     mainPanel(
+                        plotlyOutput("stateplot"), 
+                        plotOutput("deathplot"))
                      )
                  )
              ),
@@ -48,22 +48,23 @@ ui <- navbarPage(
              p("Hello, this is where I talk about my project."),
              h3("About Me"),
              p("My name is ______ and I study ______. 
-             You can reach me at ______@college.harvard.edu.")))
+             You can reach me at ______@college.harvard.edu."))
+    )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
     #tab one, render plot of cases over time
-    output$stateplot <- renderPlot({
+    output$stateplot <- renderPlotly({
     
         title <- "Confirmed coronavirus cases"
     
         # Filter the dataset based on what state was selected
         choice <- state_data_shiny %>%
-            filter(state == input$state) 
+            filter(state == input$state)
     
         #Create plot of corona cases over time
-        b <- ggplot(choice, aes(x = new_date, y = cases)) +
+        b <- ggplot(choice, aes(x = date, y = cases)) +
             geom_line() +
             geom_point() + 
             theme_minimal() +
@@ -89,13 +90,12 @@ server <- function(input, output) {
             return()
             }
         
-        
         # Filter the dataset based on what state was selected
         choice_2 <- state_data_shiny %>%
             filter(state == input$state) 
 
         #Create plot of deaths over time
-        c <- ggplot(choice_2, aes(x = new_date, y = deaths)) +
+        c <- ggplot(choice_2, aes(x = date, y = deaths)) +
             geom_line() +
             geom_point() + 
             theme_minimal() +
@@ -112,22 +112,7 @@ server <- function(input, output) {
     })
     
 
-    
-    output$info <- renderText({
-        xy_str <- function(e) {
-            if(is.null(e)) return("NULL\n")
-            paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
-        }
-        xy_range_str <- function(e) {
-            if(is.null(e)) return("NULL\n")
-            paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1), 
-                   " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
-        }
-        
-        paste0(
-            "hover: ", xy_str(input$plot_hover)
-        )
-    })
+ 
    ## screatch work from pset, rendering pre-saved plots which may be helpful later 
     output$preImage <- renderImage({
         
